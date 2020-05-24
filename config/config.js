12d5f69faf449f6e9f7d5928d05a35304021bc02
Stelios Gkiokas <Stylianos.Gkiokas@ibm.com>
2020-05-24 18:28:05 +0100

    fix: corectly retrieve accounts access token
    This commit:
    
    - fixes the README extension
    - properly retrieves the access token for a given query
    
    Signed-off-by: Stelios Gkiokas <s_giokas@hotmail.com>

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ const APP_CONFIG = {
     'CLIENT_ID': process.env.CLIENT_ID,
     'CLIENT_SECRET': process.env.CLIENT_SECRET,
     'REDIRECT_URI': process.env.REDIRECT_URI,
-    'SPOTIFY_ACCOUNTS_URI': process.env.SPOTIFY_ACCOUNTS_URI || 'accounts.spotify.com'
+    'SPOTIFY_ACCOUNTS_URI': process.env.SPOTIFY_ACCOUNTS_URI || 'accounts.spotify.com',
+    'SPOTIFY_API_URI': process.env.SPOTIFY_API_URI || 'api.spotify.com/v1',
 };
 
 module.exports = {
