c6da2e8f735cdd88e611a8efefb4a784cbaa6c52
sgkiokas <s_giokas@hotmail.com>
2020-05-24 18:31:07 +0100

    fix: corectly retrieve accounts access token (#1)
    This commit:
    
    - fixes the README extension
    - properly retrieves the access token for a given query
    
    Signed-off-by: Stelios Gkiokas <s_giokas@hotmail.com>
    
    Co-authored-by: Stelios Gkiokas <Stylianos.Gkiokas@ibm.com>

--- routes/admin.js
+++ routes/admin.js
@@ -11,27 +11,30 @@ const redirectURI = encodeURIComponent(config.APP_CONFIG.REDIRECT_URI);
 const clientSecret = config.APP_CONFIG.CLIENT_SECRET;
 const responseType = 'code';
 const requestScope = 'playlist-read-private';
+this.ACCESS_TOKEN = '';
 
-async function landing(req, res) {
+let landing = async (req, res) => {
     // immediately redirect the app to the authorize endpoint
     let authorizationURL = `https://${config.APP_CONFIG.SPOTIFY_ACCOUNTS_URI}/en/authorize?client_id=${clientID}&response_type=${responseType}&redirect_uri=${redirectURI}&scope=${requestScope}`;
     res.redirect(authorizationURL);
 }
 
-async function retrieveAccessToken(req, res) {
+let retrieveAccessToken = async (req, res) => {
     let fullURL = `${req.protocol}://${req.get('host')}${req.originalUrl}`
     let completeResponseType = `${responseType}=`;
     let responseCode = fullURL.substring(fullURL.indexOf(completeResponseType) + completeResponseType.length);
     let encodedToken = base64.encode(`${clientID}:${clientSecret}`);
-    let curlCommand = `curl -H "Authorization: Basic ${encodedToken}" -d grant_type=authorization_code -d code=${responseCode} -d redirect_uri=${redirectURI}2Fauth https://${config.APP_CONFIG.SPOTIFY_ACCOUNTS_URI}/api/token`;
+    let curlCommand = `curl -H "Authorization: Basic ${encodedToken}" -d grant_type=authorization_code -d code=${responseCode} -d redirect_uri=${redirectURI} https://${config.APP_CONFIG.SPOTIFY_ACCOUNTS_URI}/api/token`;
 
-    console.log(responseCode);
-    console.log(curlCommand);
-    let json = utils.spawnSyncWrapper(curlCommand);
-    console.log(json);
+    this.ACCESS_TOKEN = JSON.parse(utils.spawnSyncWrapper(curlCommand)).access_token;
 
-    // curl -H "Authorization: Basic NDdlMWUzZmMzYmU2NDAxM2FhMmI0NWNjMDJlYzllOTM6MjYyZDkxNjk2ZWZjNDc3OWIyYmNkNzJhNmU0NjUyNzI=" -d grant_type=authorization_code -d code=AQA1vAj8gQXQbKbAjp1SJ3hleQxUzk1dGCYcu75ShtI413xCyRA_mQqIks1KcRgTHrfjH1gbrJKKaISw3ElklihmN7PXquXIcJGXakSkeGo0fWsQFFJoxfL-axRI5dc-8BEpgLuam_cjyKWNe_x-zvu4PaG4hG9NCIdFh5V_ZS-K27NGqOTrL4_rEq
-// Vm2lRzdO1L2VbMVSqSGy7p0sYlA-BLE-rSVPKvgJE -d redirect_uri=http:%2F%2Flocalhost:8000%2Fcallback2Fauth https://accounts.spotify.com/api/token
+    // queryUser('a-user-name');
+}
+
+let queryUser = async (userName) =>  {
+    // TODO: automatically retrieve the username
+    let response = await rest.restGETRequestWrapper(config.APP_CONFIG.SPOTIFY_API_URI, userName, this.ACCESS_TOKEN, false);
+    assert.strictEqual(response.statusCode, 200, `Response code is ${response.statusCode} and not 200`);
 }
 
 router.get(endpoints.REDIRECT_URI, retrieveAccessToken);
